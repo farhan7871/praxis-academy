66839732524649dee6a0c55b423f5d15a7502a3c
farhan7871 <47026483+farhan7871@users.noreply.github.com>
2019-11-08 11:28:07 +0700

    Nomor 2

---
+++ novice/01-02/no_2.js
@@ -0,0 +1,19 @@
+var x = 3;
+var y = 1;
+var z = 2;
+
+
+if (x > 0 && y > 0 && z > 0) {
+    alert("Tandanya adalah +");
+}
+else if (x < 0 && y < 0 && z > 0) {
+    console.log("Tandanya adalah +")
+}
+else if (x > 0 && y < 0 && z < 0) {
+    console.log("Tandanya adalah +");
+}
+else if (x < 0 && y > 0 && z < 0) {
+    console.log("Tandanya adalah +");
+} else {
+    console.log("Tandanya adalah -");
+}
