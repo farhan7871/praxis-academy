e2154ffeda853dfd5a4af768549fec2695a29d36
farhan7871 <47026483+farhan7871@users.noreply.github.com>
2019-11-18 09:56:01 +0700

    Create latihan.js

---
+++ novice/01-04/latihan.js
@@ -0,0 +1,78 @@
+/*const foo = new String('foo'); // Creates a String object
+console.log(foo); // Displays: [String: 'foo']
+typeof foo; // Returns 'object'
+
+
+const firstString = '2 + 2'; // Creates a string literal value
+const secondString = new String('2 + 2'); // Creates a String object
+eval(firstString); // Returns the number 4
+eval(secondString); // Returns the string "2 + 2"
+console.log(firstString);
+console.log(secondString);
+
+console.log(`string text line 1 
+string text line 2`);
+
+const a = 10;
+const b = 15;
+
+console.log("Kamu memiliki angka 10 dan 15 ditambah menjadi " + (a + b) + " Jumlahnya");
+
+const five = 5;
+const ten = 10;
+console.log(`Fifteen is ${five + ten} and not ${2 * five + ten}.`);
+
+*/
+/*
+const msPerDay = 24 * 60 * 60 * 1000;
+ 
+// July 17, 2014 00:00:00 UTC.
+const july172014 = new Date(msPerDay * (44 * 365 + 11 + 197));
+
+const options = { year: '2-digit', month: '2-digit', day: '2-digit',
+                hour: '2-digit', minute: '2-digit', timeZoneName: 'short' };
+const americanDateTime = new Intl.DateTimeFormat('en-US', options).format;
+ 
+console.log(americanDateTime(july172014)); // 07/16/14, 5:00 PM PDT*/
+
+
+//var names = 'Harry Trump ;Fred Barney; Helen Rigby ; Bill Abel ;Chris Hand ';
+
+//console.log(names);
+
+//var re = /\s*;\s*/;
+
+//var nameList = names.split(re);
+
+//console.log(nameList);
+
+
+/*
+var nama = 'Muhammad Farhan; Agung Ukhfah; Akbarra Naufala Erdanto ; Ramadhana Bagus; Regina Putri';
+
+var keluar = ['---------------Nama-nama berikut adalah\n', nama + '\n'];
+
+var re = /\s*;\s*/;
+/*
+var listnama = nama.split(re);
+
+re = /(\w+)\s+(\w+)/;
+
+var bynama = [];
+
+
+var i,len;
+for(i = 0, len = listnama.length; i < len; i++){
+    output.push(listnama[i]);
+    bynama[i] = listnama[i].replace(pattern, '$2, $1');
+}
+
+output.push('-----------Perubahan');
+for( i = 0, len = listnama.length; i < len; i++){
+    output.push(listnama[i]);
+}
+
+output.push('-----------Selesai');
+
+console.log(output.join('\n'));
+*/
