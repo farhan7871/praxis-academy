411d59fd5e1b85d2a8e75211378fa7174e07d275
farhan7871 <47026483+farhan7871@users.noreply.github.com>
2019-11-18 09:56:39 +0700

    Create kasus.js

---
+++ novice/01-04/kasus.js
@@ -0,0 +1,40 @@
+// Soal Nomor 1
+
+/*var names = 'Muhammad Farhan ;Albarra Naufala; Agung Ukhfah ; Regina Putri ; Ramadhana Bagus ';
+
+var output = ['---------- Original String\n', names + '\n'];
+
+var pattern = /\s*;\s*/;
+
+/*var nameList = names.split(pattern);
+
+pattern = /(\w+)\s+(\w+)/;
+
+var bySurnameList = [];
+
+output.push('---------- After Split by Regular Expression');
+
+var i, len;
+for (i = 0, len = nameList.length; i < len; i++) {
+  output.push(nameList[i]);
+  bySurnameList[i] = nameList[i].replace(pattern, '$2, $1');
+}
+
+// Display the new array.
+output.push('---------- Names Reversed');
+for (i = 0, len = bySurnameList.length; i < len; i++) {
+  output.push(bySurnameList[i]);
+}
+
+// Sort by last name, then display the sorted array.
+bySurnameList.sort();
+output.push('---------- Sorted');
+for (i = 0, len = bySurnameList.length; i < len; i++) {
+  output.push(bySurnameList[i]);
+}
+
+output.push('---------- End');
+
+console.log(output.join('\n'));*/
+
+//Soal Nomor 2
